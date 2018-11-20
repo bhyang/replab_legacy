0565afa23d8f226e42720d59aedb244806c2fa61
Brian Yang <brian@Brians-MacBook-Pro.local>
2018-11-20 13:16:25 -0800

    Initial commit

---
+++ src/controller.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+#
+# Custom controller that uses MoveIt for ef control for widowX arm at RAIL
+# Author: Dian Chen (dianchen@berkeley.edu)
+
+import rospy
+from geometry_msgs.msg import (
+    PointStamped,
+    Point,
+    Quaternion,
+    Pose,
+)
+from moveit_commander import *
+from moveit_msgs.srv import *
+from arbotix_msgs.srv import *
+
+from moveit_commander.exception import MoveItCommanderException
+
+import numpy as np
+import pickle
+import traceback
+
+from config import *
+
+
+class WidowX:
+
+    def __init__(self):
+        self.scene = PlanningSceneInterface()
+        self.commander = MoveGroupCommander("widowx_arm")
+        self.gripper = MoveGroupCommander("widowx_gripper")
+
+        self.commander.set_end_effector_link('gripper_rail_link')
+
+        rospy.sleep(2)
+
+    def open_gripper(self, drop=False):
+        plan = self.gripper.plan(GRIPPER_DROP if drop else GRIPPER_OPEN)
+        return self.gripper.execute(plan, wait=True)
+
+    def close_gripper(self):
+        plan = self.gripper.plan(GRIPPER_CLOSED)
+        return self.gripper.execute(plan, wait=True)
+
+    def eval_grasp(self, threshold=.001):
+        current = np.array(self.gripper.get_current_joint_values())
+        target = np.array(GRIPPER_CLOSED)
+        error = current[0] - target[0]
+        return error > threshold, error
+
+    def orient_to_target(self, x=None, y=None, angle=None):
+        current = self.get_joint_values()
+        if x or y:
+            angle = np.arctan2(y, x)
+        if angle >= 3.1:
+            angle = 3.1
+        elif angle <= -3.1:
+            angle = -3.1
+        current[0] = angle
+        # current[3] = -1.57
+        plan = self.commander.plan(current)
+        return self.commander.execute(plan, wait=True)
+
+    def wrist_rotate(self, angle):
+        rotated_values = self.commander.get_current_joint_values()
+        rotated_values[4] = angle - rotated_values[0]
+        if rotated_values[4] > np.pi / 2:
+            rotated_values[4] -= np.pi
+        elif rotated_values[4] < -(np.pi / 2):
+            rotated_values[4] += np.pi
+        plan = self.commander.plan(rotated_values)
+        return self.commander.execute(plan, wait=True)
+
+    def get_joint_values(self):
+        return self.commander.get_current_joint_values()
+
+    def get_current_pose(self):
+        return self.commander.get_current_pose()
+
+    def move_to_neutral(self):
+        print('Moving to neutral...')
+        plan = self.commander.plan(NEUTRAL_VALUES)
+        return self.commander.execute(plan, wait=True)
+
+    def move_to_drop(self, angle=None):
+        drop_positions = DROPPING_VALUES[:]
+        if angle:
+            drop_positions[0] = angle
+        plan = self.commander.plan(drop_positions)
+        return self.commander.execute(plan, wait=True)
+
+    def move_to_empty(self):
+        plan = self.commander.plan(EMPTY_VALUES)
+        return self.commander.execute(plan, wait=True)
+
+    def move_to_reset(self):
+        print('Moving to reset...')
+        plan = self.commander.plan(RESET_VALUES)
+        return self.commander.execute(plan, wait=True)
+
+    def orient_to_pregrasp(self, x, y):
+        angle = np.arctan2(y, x)
+        return self.move_to_drop(angle)
+
+    def move_to_grasp(self, x, y, z, angle):
+        current_p = self.commander.get_current_pose().pose
+        p1 = Pose(position=Point(x=x, y=y, z=z), orientation=DOWN_ORIENTATION)
+        plan, f = self.commander.compute_cartesian_path(
+            [current_p, p1], 0.001, 0.0)
+
+        joint_goal = list(plan.joint_trajectory.points[-1].positions)
+
+        first_servo = joint_goal[0]
+
+        joint_goal[4] = (angle - first_servo) % np.pi
+        if joint_goal[4] > np.pi / 2:
+            joint_goal[4] -= np.pi
+        elif joint_goal[4] < -(np.pi / 2):
+            joint_goal[4] += np.pi
+
+        try:
+            plan = self.commander.plan(joint_goal)
+        except MoveItCommanderException as e:
+            print('Exception while planning')
+            traceback.print_exc(e)
+            return False
+
+        return self.commander.execute(plan, wait=True)
+
+    def move_to_vertical(self, z, force_orientation=True, shift_factor=1.0):
+        current_p = self.commander.get_current_pose().pose
+        current_angle = self.get_joint_values()[4]
+        orientation = current_p.orientation if force_orientation else None
+        p1 = Pose(position=Point(x=current_p.position.x * shift_factor,
+                                 y=current_p.position.y * shift_factor, z=z), orientation=orientation)
+        waypoints = [current_p, p1]
+        plan, f = self.commander.compute_cartesian_path(waypoints, 0.001, 0.0)
+
+        if not force_orientation:
+            return self.commander.execute(plan, wait=True)
+        else:
+            if len(plan.joint_trajectory.points) > 0:
+                joint_goal = list(plan.joint_trajectory.points[-1].positions)
+            else:
+                return False
+
+            joint_goal[4] = current_angle
+
+            plan = self.commander.plan(joint_goal)
+            return self.commander.execute(plan, wait=True)
+
+    def move_to_target(self, target):
+        plan = self.commander.plan(target)
+        return self.commander.execute(plan, wait=True)
+
+    def sweep_arena(self):
+        self.move_to_drop(.8)
+        plan = self.commander.plan(TL_CORNER[0])
+        self.commander.execute(plan, wait=True)
+
+        plan = self.commander.plan(TL_CORNER[1])
+        self.commander.execute(plan, wait=True)
+
+        plan = self.commander.plan(L_SWEEP[0])
+        self.commander.execute(plan, wait=True)
+
+        plan = self.commander.plan(L_SWEEP[1])
+        self.commander.execute(plan, wait=True)
+
+        self.move_to_drop(-.8)
+        plan = self.commander.plan(BL_CORNER[0])
+        self.commander.execute(plan, wait=True)
+
+        plan = self.commander.plan(BL_CORNER[1])
+        self.commander.execute(plan, wait=True)
+
+        self.move_to_drop(-2.45)
+        plan = self.commander.plan(BR_CORNER[0])
+        self.commander.execute(plan, wait=True)
+        plan = self.commander.plan(BR_CORNER[1])
+        self.commander.execute(plan, wait=True)
+
+        self.move_to_drop(2.3)
+        plan = self.commander.plan(TR_CORNER[0])
+        self.commander.execute(plan, wait=True)
+        plan = self.commander.plan(TR_CORNER[1])
+        self.commander.execute(plan, wait=True)
+
+    def discard_object(self):
+        plan = self.commander.plan(PREDISCARD_VALUES)
+        self.commander.execute(plan, wait=True)
+        plan = self.commander.plan(DISCARD_VALUES)
+        self.commander.execute(plan, wait=True)
+        self.open_gripper(drop=True)
+        plan = self.commander.plan(PREDISCARD_VALUES)
+        self.commander.execute(plan, wait=True)
+        self.move_to_neutral()
+
+
+def main():
+    rospy.init_node("widowx_custom_controller")
+    widowx = WidowX()
+
+    print('For debugging purposes')
+    import pdb; pdb.set_trace()
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ src/human_executor.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import (
+    Image,
+    PointCloud2,
+    CameraInfo
+)
+from std_msgs.msg import (
+    UInt16,
+    Int64,
+    Float32,
+    Header,
+    String
+)
+from image_geometry import PinholeCameraModel
+from cv_bridge import CvBridge, CvBridgeError
+
+import argparse
+import traceback
+
+from controller import WidowX
+from policy import *
+from config import *
+from utils import *
+
+
+class HumanExecutor:
+
+    def __init__(self, execute=False, calibrate=False):
+        # WidowX controller interface
+        self.widowx = WidowX()
+
+        # Register subscribers
+        self.pc_subscriber = rospy.Subscriber(
+            "/camera/depth/points", PointCloud2, self.update_pc)
+        self.camerainfo_subscriber = rospy.Subscriber(
+            "/camera/depth/camera_info", CameraInfo, self.save_caminfo)
+        self.action_subscriber = rospy.Subscriber(
+            "/target_action", String, self.take_action)
+        self.rgb_subscriber = rospy.Subscriber(
+            "/camera/rgb/image_raw", Image, self.update_rgb)
+
+        # Store latest RGB-D
+        self.rgb = None
+        self.depth = None
+        self.pc = None
+
+        self.bridge = CvBridge()
+
+        self.cm = CALIBRATION_MATRIX
+
+        self.robot_coordinates = []
+        self.camera_coordinates = []
+
+        self.execute = execute
+        self.calibrate = calibrate
+
+    def update_rgb(self, data):
+        self.rgb = self.bridge.imgmsg_to_cv2(data)
+
+    def update_d(self, data):
+        self.depth = self.bridge.imgmsg_to_cv2(data)
+
+    def update_pc(self, data):
+        self.pc = list(pc2.read_points(data, skip_nans=False))
+
+    def save_caminfo(self, data):
+        self.cam_info = data
+
+    def take_action(self, data):
+        x, y = data.data.split('(')[1].split(')')[0].split(',')
+        register_index = int(x) + (int(y) * 640)
+        pc_point = list(self.pc[register_index])
+        # pc_depth = pc_point[2]
+        # depth = self.depth
+        # pc_point[2] = depth[int(y), int(x)] / 1000.
+
+        # import pdb; pdb.set_trace()
+
+        # img = np.reshape(self.pc, (480, 640, 3))
+        # img = np.sum(img, axis=2)
+
+        # plt.imshow(img)
+        # plt.show()
+
+        print('Pixel: (%s, %s)' % (x, y))
+        print('PC point: ' + str(pc_point))
+
+        if pc_point[2] == 0:
+            print('No depth reading')
+            print(pc_depth)
+            return
+
+        if self.execute:
+            pc_point = np.array([pc_point[0], pc_point[1], pc_point[2], 1.])
+            transformed = np.dot(self.cm, pc_point)[:3]
+            print(transformed)
+
+            theta = 0.0
+            grasp = np.concatenate([transformed, [theta]], axis=0)
+            grasp[2] -= Z_OFFSET
+            grasp[:2] *= CONTROL_NOISE_COEFFICIENT
+
+            print('Grasp: ' + str(grasp))
+            success, actual_grasp = self.execute_grasp(grasp)
+            self.widowx.move_to_neutral()
+
+        elif self.calibrate:
+            user_in = raw_input('Keep recorded point? [y/n] ')
+
+            if user_in == 'y':
+                pose = self.widowx.get_current_pose().pose.position
+                print(pose.x, pose.y, pose.z)
+
+                self.robot_coordinates.append((pose.x, pose.y, pose.z))
+                self.camera_coordinates.append(pc_point[:3])
+                np.save('robot_points', np.array(self.robot_coordinates)) ###########################
+                np.save('camera_points', np.array(self.camera_coordinates)) ######################### DELETE LATER
+                print('Saved')
+
+            elif user_in == 'n':
+                print('Not saved')
+
+    def get_pose(self):
+        pose = self.widowx.get_current_pose().pose
+        pose_list = [pose.position.x,
+                     pose.position.y,
+                     pose.position.z,
+                     pose.orientation.w,
+                     pose.orientation.x,
+                     pose.orientation.y,
+                     pose.orientation.z]
+        return pose_list
+
+    def execute_grasp(self, grasp):
+        try:
+            x, y, z, theta = grasp
+            print('Attempting grasp: (%.4f, %.4f, %.4f, %.4f)'
+                  % (x, y, z, theta))
+
+            assert self.widowx.orient_to_target(
+                x, y), 'Failed to orient to target'
+
+            assert self.widowx.move_to_grasp(x, y, z, theta), \
+                'Failed to reach target'
+
+            self.widowx.close_gripper()
+
+            pose = self.get_pose()
+            joints = self.widowx.get_joint_values()
+
+            actual_grasp = pose[:3] + [joints[4]]
+
+            print('Actual grasp: (%.4f, %.4f, %.4f, %.4f)' % (
+                actual_grasp[0], actual_grasp[1], actual_grasp[2], actual_grasp[3]))
+
+            reached = self.widowx.move_to_vertical(z - GRASP_LIFT_CLEARANCE)
+            offset = 0.0
+            while not reached and offset < GRASP_LIFT_CLEARANCE:
+                offset += .005
+                reached = self.widowx.move_to_vertical(
+                    z - GRASP_LIFT_CLEARANCE + offset)
+
+            assert self.widowx.move_to_drop(), 'Failed to move to drop'
+
+            success, err = self.widowx.eval_grasp()
+
+            return success, actual_grasp
+
+        except AssertionError as e:
+            print('Error executing grasp -- returning...')
+            traceback.print_exc(e)
+            return False, None
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Executes user-specified grasps from a GUI window")
+    parser.add_argument('--debug', action="store_true", default=False,
+                        help="Prevents grasp from being executed (for debugging purposes)")
+    args = parser.parse_args()
+
+    rospy.init_node("human_executor")
+
+    executor = HumanExecutor(execute=(not args.debug), calibrate=False)
+    print('Run commander_human.py to issue commands from the GUI window')
+
+    rospy.sleep(2)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
